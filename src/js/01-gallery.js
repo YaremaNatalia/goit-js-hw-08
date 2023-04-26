114225081df0b207bc4366df1dbb57439ad70ef9
YaremaNatalia <yaremka.n@gmail.com>
2023-04-26 18:46:00 +0300

    fix notifications

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery'); // отримуємо посилання на галерею
+
+const imagesMarkup = createPhotosMarkup(galleryItems); // записуємо в константу виклик функції створення розмітки з вкладенням масиву картинок
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup); // додаємо розмітку в контейнер HTML
+
+function createPhotosMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>
+    `;
+    })
+    .join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt', // звернення до опцій бібліотеки (додавання тексту і часу появи)
+  captionDelay: 250,
+});
