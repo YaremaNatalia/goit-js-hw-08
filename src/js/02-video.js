f976213ab3202289eb66fe04c970a5ab0eb12fd5
YaremaNatalia <yaremka.n@gmail.com>
2023-05-01 19:44:35 +0300

    fixes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,51 +6,21 @@ const STORAGE_KEY = 'videoplayer-current-time';
 const idPlayer = document.querySelector('#vimeo-player'); // підключення плеєра
 const player = new Player(idPlayer);
 
-resumePlayBack();
+resumePlayBack(); // виклик функції відновлення відео з місця зупинки після перезавантаження сторінки
 
 // за допомогою методу плеєра (events - події) відстежуємо під час програвання плеєра подію timeupdate - оновлення часу відтворення
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay(timeData) {
-  const currentTime = JSON.stringify(timeData); // сторюємо константу для збереженнячасу відтворення відео плеєра(обєкт приходить з player.on('timeupdate'), приводимо до строки
-  localStorage.setItem(STORAGE_KEY, currentTime); // Зберігаємо  у локальне сховище
+// через деструктирізацію витягаємо з обекта тільки секунди
+function onPlay({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds); // Зберігаємо  у локальне сховище секунти без приведення до строки бо це прості дані(числа)
 }
 
 // функція відновлення відео з місця зупинки після перезавантаження сторінки
 function resumePlayBack() {
-  const currentTimeData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const currentTimeData = localStorage.getItem(STORAGE_KEY);
   // перевіряємо чи є збережене в localStorage
   if (currentTimeData) {
-    player
-      .setCurrentTime(currentTimeData.seconds) // повертаємо остані введені дані з обєкта локал сторедж
-      .catch(function (error) {
-        switch (error.name) {
-          case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-          default:
-            // some other error occurred
-            break;
-        }
-      });
+    player.setCurrentTime(currentTimeData); // повертаємо остані введені дані з обєкта локал сторедж
   }
 }
-
-// const currentTimeData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-// // перевіряємо чи є збережене в localStorage
-// if (currentTimeData) {
-//   player
-//     .setCurrentTime(currentTimeData.seconds) // повертаємо остані введені дані з обєкта локал сторедж (звертаємось до параметру обєкта "секунди")
-//     .catch(function (error) {
-//       switch (error.name) {
-//         case 'RangeError':
-//           // the time was less than 0 or greater than the video’s duration
-//           break;
-
-//         default:
-//           // some other error occurred
-//           break;
-//       }
-//     });
-// }
--- src/js/03-functions.js
+++ src/js/03-functions.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 import refs from './03-references'; // дефолтне імпортування посилань на елементи форми
 
 const STORAGE_KEY = 'feedback-form-state'; // виносимо в глобальну змінну параметр, що часто використовується
-const formData = {}; // створюємо обєкт для збереження тексту з інпутів
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {}; // створюємо обєкт для збереження тексту з інпутів, якщо щось було вже прописано при перезавантаженні залишаємо в локалстореджі і додаємо до нього текст далі (також додаємо в інпут),якщо нічого не було (null) буде пустий масив
 
 // функція запису в локал сторедж значень інпутів
 function onTextAreaInputs(event) {
@@ -15,7 +15,7 @@ function onTextAreaInputs(event) {
 function populateInputsArea() {
   // let savedInputsData;
   const stringData = localStorage.getItem(STORAGE_KEY); // отримання обєкту через ключ у вигляді строки
-   const savedInputsData = JSON.parse(stringData); // персел строки в обєкт
+  const savedInputsData = JSON.parse(stringData); // персел строки в обєкт
   // перевірка на помилки при парселі даних з локал стореджу
   // try {
   //   const stringData = localStorage.getItem(STORAGE_KEY); // отримання обєкту через ключ у вигляді строки
@@ -31,11 +31,16 @@ function populateInputsArea() {
 }
 // функція очищення форми, локал стореджу і виведення останніх даних в консоль при натисканні сабміту
 function onSubmitBtn(event) {
-  event.preventDefault(); // уникнення перезавантаження сторінки
-  event.currentTarget.reset(); //очищення всієї форми при кліку на кнопку (резет лише якщо це форма)
-  // refs.currentTarget.value = ''; // якщо не форма можна очищати значення  полів через присвоєння пустої строки
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY))); // виведення у консоль об'єкт з полями email, message та їхніми поточними значеннями
-  localStorage.removeItem(STORAGE_KEY); // очищення локал стореджу після сабміту
+  if ((refs.email.value !== '') & (refs.message.value !== '')) {
+    event.preventDefault(); // уникнення перезавантаження сторінки
+    event.currentTarget.reset(); //очищення всієї форми при кліку на кнопку (резет лише якщо це форма)
+    // refs.currentTarget.value = ''; // якщо не форма можна очищати значення  полів через присвоєння пустої строки
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY))); // виведення у консоль об'єкт з полями email, message та їхніми поточними значеннями
+    localStorage.removeItem(STORAGE_KEY); // очищення локал стореджу після сабміту
+    formData = {}; // очищення обєкту після сабміту
+  } else {
+    alert('Заповніть, будь ласка, всі поля');
+  }
 }
 
 export { onTextAreaInputs, populateInputsArea, onSubmitBtn }; //іменований експорт функцій через обєкт
